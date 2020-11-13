7b37a3beb952640724669d2cd56bc24b5ea3b5d6
gus25 <74336047+gus25@users.noreply.github.com>
2020-11-13 13:56:41 -0500

    Update j1.js

--- ja/j1.js
+++ ja/j1.js
@@ -1,1 +1,10 @@
 
+let div1 = document.getElementById('titulogrupo1');
+
+ 
+for (var k = 1 ; k <= 35; k++) {
+
+	div1.innerHTML += 
+    '<div class="imagen"><img alt="" id="img'+k+'" width="100%" /></div>'+
+	'<div class="nombregrupo"><a id="a'+k+'" width="auto"> </a></div>';  
+ }  
