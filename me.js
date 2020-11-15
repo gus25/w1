26ab9f116e4fc4fee8c89b6720a3efe998bb2253
gus25 <74336047+gus25@users.noreply.github.com>
2020-11-14 22:27:56 -0500

    Update me.js

--- me.js
+++ me.js
@@ -1,11 +1,10 @@
-let divmenu = document.getElementById('menu');
+  document.write('<nav><ul>');
+  document.write('<li><a href="https://gus25.github.io/w1/">Inicio</a>');	
+  document.write('<li><a href="#">grupos</a><ul>');
 
-divmenu.innerHTML += '<nav><ul>'+
-'<li><a href="https://gus25.github.io/w1/">Inicio</a>'+
- '<li><a href="#">grupos</a><ul>';
- 
 for (var k = 1 ; k <= 35; k++) {
 
-	divmenu.innerHTML += '<li><a href="https://gus25.github.io/w1/g'+k+'.html">grupo'+k+'</a></li>';
-}
-divmenu.innerHTML +='</ul> </li></ul> </nav>';
+	document.write('<li><a href="https://gus25.github.io/w1/g'+k+'.html">grupo'+k+'</a></li>');
+}	
+	
+	document.write('</ul> </li></ul> </nav>');
