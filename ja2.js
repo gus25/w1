5eda80fa90610a871d695e495e392560a560e47d
gus25 <74336047+gus25@users.noreply.github.com>
2020-11-14 20:03:38 -0500

    Update ja2.js

--- ja2.js
+++ ja2.js
@@ -1,14 +1,3 @@
-let divmenu = document.getElementById('menu');
-
-divmenu.innerHTML += '<nav><ul>'+
-'<li><a href="https://gus25.github.io/w1/">Inicio</a>'+
- '<li><a href="#">grupos</a><ul>';
- 
-for (var k = 1 ; k <= 35; k++) {
-
-	divmenu.innerHTML += '<li><a href="https://gus25.github.io/w1/g'+k+'.html">grupo'+k+'</a></li>';
-}
-divmenu.innerHTML +='</ul> </li></ul> </nav>';
 
 
 //let n=1; 
