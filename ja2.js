025699db5fb3af1a6e811a29d46b847f2166793f
gus25 <74336047+gus25@users.noreply.github.com>
2021-02-04 19:18:29 -0500

    Update ja2.js

--- ja2.js
+++ ja2.js
@@ -266,19 +266,19 @@ document.getElementById('txt38').innerHTML = 	"Como cargar la BATERÍA DE TU AUT
 	
 											
 document.getElementById("img39").src= "im/v39.png";
-document.getElementById('txt39').innerHTML = 	"Tutorial Electrónica básica TRANSISTORES<br />"+
+document.getElementById('txt39').innerHTML = 	"Tutorial Electrónica básica sobre TRANSISTORES Di Gracias y ten aqui el video<br />"+
 												"https://youtu.be/lN0AfX0XI8s";
 
 document.getElementById("img40").src= "im/v40.png";
-document.getElementById('txt40').innerHTML =	"Tutorial básico sobre resistencias y capacitores<br />"+
+document.getElementById('txt40').innerHTML =	"Tutorial básico sobre resistencias y capacitores Di Gracias y ten aqui el video<br />"+
 												"https://youtu.be/vJnGSqnFgvw";											
 
 document.getElementById("img41").src= "im/v41.png";
-document.getElementById('txt41').innerHTML =	"Tutorial electrónica básica REGULADORES <br />"+
+document.getElementById('txt41').innerHTML =	"Tutorial electrónica básica REGULADORES Di Gracias y ten aqui el video<br />"+
 												"https://youtu.be/lN0AfX0XI8s";											
 											
 document.getElementById("img42").src= "im/v42.png";
-document.getElementById('txt42').innerHTML = 	"Tutorial de electrónica Inductores y diodos <br />"+
+document.getElementById('txt42').innerHTML = 	"Tutorial de electrónica Inductores y diodos Di Gracias y ten aqui el video<br />"+
 												"https://youtu.be/vJnGSqnFgvw";											
 											
 document.getElementById("img43").src= "im/v43.png";
